33188a14c3eba76614cc89dbecf39983a393affe
Swamytirumalasetti <132669288+Swamytirumalasetti@users.noreply.github.com>
2023-05-05 17:27:03 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,14 @@
+let userName = "Swamy"
+console.log( userName)
+
+let userCity = "Machilipatnam"
+console.log(userCity)
+
+let userHobbies = ["Playing Games",'Learning new things',`Travelling`,"Music"]
+console.log(userHobbies)
+
+let num = 1234
+console.log(typeof num)
+
+let currentdate = new Date
+console.log(currentdate)
